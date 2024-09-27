2de193c836daca8dccbaddd1b84ed4f81da86bf4
florent2009 <33114936+florent2009@users.noreply.github.com>
2024-09-27 13:11:40 +0000

    #

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,10 +68,3 @@ if query:
 else:
     st.write("Start typing to search for books...")
 
-hide_streamlit_style = """
-            <style>
-            [data-testid="stToolbar"] {visibility: hidden !important;}
-            footer {visibility: hidden !important;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
